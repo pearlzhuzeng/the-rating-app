91e45de0f00d8f0e53637869f3bf8b8d1681b148
Pearl Zeng <zhuzeng@umich.edu>
2019-02-05 12:42:57 -0500

    Improve test for viewing all recipes

--- spec/system/viewing_recipes_spec.rb
+++ spec/system/viewing_recipes_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Viewing recipes' do
   it 'shows all recipes' do
     create :recipe, title: "Old Friend Noodle"
+    create :recipe, title: "Kongpao Chicken"
 
     visit root_path
 
     expect(page).to have_content "Old Friend Noodle"
+    expect(page).to have_content "Kongpao Chicken"
   end
 end
