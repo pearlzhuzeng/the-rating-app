8dcc2656c4c8c0123183d7e755cb59d38c4db564
Pearl Zeng <zhuzeng@umich.edu>
2019-02-11 14:26:22 -0500

    Move clearance routes to before catch all route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,23 @@ class ReachRouterConstraint
   end
 end
 
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   root to: 'home#show'
+
+  # Clearance routes
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
   
   # expect 'api/' prefix to prevent conflict between rails/react routes
   scope :api do
