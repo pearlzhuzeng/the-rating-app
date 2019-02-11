8c49bb142526709f7491e40c103e37d5c5aec83d
Pearl Zeng <zhuzeng@umich.edu>
2019-02-11 13:02:26 -0500

    Use Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_29_225349) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recipes", force: :cascade do |t|
     t.string "title"
   end
