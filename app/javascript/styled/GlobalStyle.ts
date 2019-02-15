d4f24d0658ec4ff035b7c4073d025bfa610e4a47
Pearl Zeng <zhuzeng@umich.edu>
2019-02-14 22:51:10 -0500

    Update Global Style

--- app/javascript/styled/GlobalStyle.ts
+++ app/javascript/styled/GlobalStyle.ts
@@ -3,10 +3,27 @@ import { createGlobalStyle } from '../typed/styled-components'
 export default createGlobalStyle`
 html {
   @import url('https://fonts.googleapis.com/css?family=Lato');
+  @import url('https://fonts.googleapis.com/css?family=Merriweather')
   background-color: ${p => p.theme.primaryColor};
   font-family: 'Lato', sans-serif;
   margin: 0 auto;
   margin-top: 2em;
   max-width: 60em;
+}
+
+a {
+  color: #136d88
+}
+
+button {
+  background-color: #fffffe;
+  color: #555;
+  border-radius: 0.3em;
+
+  &:hover {
+    background-color: #136d88;
+    color: white;
+    transition: all 0.1s;
+  }
 }
 `
