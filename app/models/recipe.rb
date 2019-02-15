2d1f9a1ae77043f6a3d884ba9ef1ac3b960099b6
Pearl Zeng <zhuzeng@umich.edu>
2019-02-14 22:41:21 -0500

    Add number_of_reviews and scores for the five criteria to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,46 @@
 class Recipe < ApplicationRecord
   has_many :reviews
 
-  def average_score
+
+  def average_overall_score
     return 0 if reviews.none?
     
     reviews.sum(&:overall) / reviews.length.to_f
+  end
+
+  def number_of_reviews
+    return 0 if reviews.none?
+
+    reviews.length
+  end
+
+  def average_taste_score
+    return 0 if reviews.none?
+    
+    reviews.sum(&:taste) / reviews.length.to_f
+  end
+
+  def average_appearance_score
+    return 0 if reviews.none?
+    
+    reviews.sum(&:appearance) / reviews.length.to_f
+  end
+
+  def average_ease_score
+    return 0 if reviews.none?
+    
+    reviews.sum(&:ease) / reviews.length.to_f
+  end
+
+  def average_time_score
+    return 0 if reviews.none?
+    
+    reviews.sum(&:time) / reviews.length.to_f
+  end
+
+  def average_cost_score
+    return 0 if reviews.none?
+    
+    reviews.sum(&:cost) / reviews.length.to_f
   end
 end
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,3 @@
 class RecipeSerializer < ApplicationSerializer
-  attributes :title, :detail, :average_score
+  attributes :title, :detail, :average_overall_score, :number_of_reviews, :average_taste_score, :average_appearance_score, :average_ease_score, :average_time_score, :average_cost_score
 end
