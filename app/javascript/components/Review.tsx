7d64337db0063c46a75004ec870e586d64f2d1e4
Pearl Zeng <zhuzeng@umich.edu>
2019-02-14 22:29:25 -0500

    Style individual reviews

--- app/javascript/components/Review.tsx
+++ app/javascript/components/Review.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from '../typed/styled-components'
 
 import { RatingValues } from './RatingField'
 
@@ -11,14 +12,99 @@ type Props = {
 export default function Review({ review }: Props) {
   debugger
   return (
-    <div>
-      <p>Name: {review.name} </p>
-      <p>Taste: {RatingValues.get(review.taste)} </p>
-      <p>Appearance: {RatingValues.get(review.appearance)} </p>
-      <p>Ease: {RatingValues.get(review.ease)} </p>
-      <p>Time: {RatingValues.get(review.time)} </p>
-      <p>Cost: {RatingValues.get(review.cost)} </p>
-      <p>Comment: {review.comment} </p>
-    </div>
+    <Container>
+      <ReviewContainer>
+        <RatingsContainer>
+          <RatingLabel>Taste:</RatingLabel>
+          <RatingValue>
+            <VisuallyHidden>{RatingValues.get(review.taste)}</VisuallyHidden>
+            <Stars n={review.taste} />
+          </RatingValue>
+
+          <RatingLabel>Appearance:</RatingLabel>
+          <RatingValue>
+            <VisuallyHidden>
+              {RatingValues.get(review.appearance)}
+            </VisuallyHidden>
+            <Stars n={review.appearance} />
+          </RatingValue>
+
+          <RatingLabel>Ease:</RatingLabel>
+          <RatingValue>
+            <VisuallyHidden>{RatingValues.get(review.ease)}</VisuallyHidden>
+            <Stars n={review.ease} />
+          </RatingValue>
+
+          <RatingLabel>Time:</RatingLabel>
+          <RatingValue>
+            <VisuallyHidden>{RatingValues.get(review.time)}</VisuallyHidden>
+            <Stars n={review.time} />
+          </RatingValue>
+
+          <RatingLabel>Cost:</RatingLabel>
+          <RatingValue>
+            <VisuallyHidden>{RatingValues.get(review.cost)}</VisuallyHidden>
+            <Stars n={review.cost} />
+          </RatingValue>
+        </RatingsContainer>
+        <p>{review.comment} </p>
+      </ReviewContainer>
+      <p>By: {review.name}</p>
+    </Container>
   )
 }
+
+function Stars({ n }: { n: number }) {
+  return (
+    <>
+      {new Array(n).fill(<FilledStar>★</FilledStar>)}
+      {new Array(5 - n).fill(<EmptyStar>★</EmptyStar>)}
+    </>
+  )
+}
+
+const Container = styled.div`
+  backgroud-color: #f9f8f2;
+  border-bottom: solid 1px #ddd;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 1em;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`
+
+const ReviewContainer = styled.div`
+  flex-basis: 80%;
+  padding-right: 1em;
+`
+
+const RatingsContainer = styled.dl`
+  display: grid;
+  grid-template-columns: min-content 1fr;
+`
+
+const RatingLabel = styled.dt`
+  display: inline-block;
+`
+
+const RatingValue = styled.dd`
+  display: inline-block;
+`
+
+const VisuallyHidden = styled.span`
+  position: absolute;
+  left: -999999px;
+`
+
+const FilledStar = styled.span`
+  color: orange;
+  padding-right: 0.2em;
+`
+
+const EmptyStar = styled.span`
+  color: #ddd;
+  padding-right: 0.2em;
+`
